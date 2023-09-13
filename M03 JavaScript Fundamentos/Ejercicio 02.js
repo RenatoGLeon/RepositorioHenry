5289f779e1b3c7ebd992bc098abe57046476662e
RenatoGLeon <118228216+RenatoGLeon@users.noreply.github.com>
2023-09-13 08:01:30 -0500

    Add files via upload

---
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -0,0 +1,50 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*2️⃣ EJERCICIO 02 2️⃣*/
+
+function devolverString(string) {
+   return string
+   // Debe retornar un string.
+   // Tu código:
+}
+
+// ⛔️ "X" e "Y" son números.
+
+function suma(x, y) {
+   return x + y
+   // Retorna el resultado de su suma.
+   // Tu código:
+}
+
+function resta(x, y) {
+   return x - y
+   // Retorna el resultado de la resta.
+   // Tu código:
+}
+
+function divide(x, y) {
+   return x / y
+   // Retorna el resultado de su división.
+   // Tu código:
+}
+
+function multiplica(x, y) {
+   return x * y
+   // Retorna el resultado de su multiplicación.
+   // Tu código:
+}
+
+function obtenerResto(x, y) {
+   return x % y
+   // Obten el resto de la división de "x" entre "y".
+   // Tu código:
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   devolverString,
+   suma,
+   resta,
+   divide,
+   multiplica,
+   obtenerResto,
+};
